89c4193acb85dd855665304b73b9b8a0500255b6
austinkelsay <austinkelsay@yahoo.com>
2021-12-11 16:36:31 -0600

    Getting rid of prepared option

--- gpt3/openai_connector.py
+++ gpt3/openai_connector.py
@@ -4,17 +4,10 @@ import openai
 from dotenv import load_dotenv
 
 class OpenAIConnector:
-    def __init__(self, token: str, filepath: str, model_name: str, prepare: bool = True, prepared_filepath: str = None):
+    def __init__(self, token: str, filepath: str, model_name: str):
         self.filepath = filepath
         self.model_name = model_name
         self.token = token
-        self.prepare = prepare
-        self.prepared_filepath = prepared_filepath
-
-    def prepare_fine_tune_dataset(self):
-        # Runs the open ai fine tune dataset preparation CLI script on the dataset usine the instantiated filepath
-        prepare = subprocess.run(["openai", "tools", "fine_tunes.prepare_data", "-f", self.filepath], capture_output=True)
-        print(prepare.stdout.decode("utf-8"))
 
     def upload_dataset(self):
         load_dotenv()
