e21ea77da53cff878c3e760244a2f068188ecdd4
austinkelsay <austinkelsay@yahoo.com>
2021-12-11 17:45:50 -0600

    Added create_fine_tune method to openai connector and also cleaned up the upload method

--- gpt3/openai_connector.py
+++ gpt3/openai_connector.py
@@ -16,6 +16,24 @@ class OpenAIConnector:
         file=open(self.prepared_filepath),
         purpose='fine-tune'
         )
+        openai_file_list = openai.File.list()
+        # Sort file list by date (reversed)
+        sorted_files = sorted(openai_file_list['data'], key=lambda d: d['created_at'], reverse=True)
+        new_file = sorted_files[0]['id']
+        print(new_file)
 
-test = OpenAIConnector(token="", filepath="./datasets/openai_datasets/bitcoin_chatbot_training_data.jsonl", model_name="", prepare=True, prepared_filepath="")
-test.prepare_fine_tune_dataset()
+        return new_file
+
+    def create_fine_tune_model(self):
+        load_dotenv()
+        openai_training_file = os.getenv("TRAINING_FILE")
+        openai.FineTune.create(training_file=openai_training_file, model="curie", n_epochs=5)
+
+        # print(openai.FineTune.list())
+        openai_fine_tune_list = openai.FineTune.list()
+        sorted_fine_tunes = sorted(openai_fine_tune_list['data'], key=lambda d: d['created_at'], reverse=True)
+        # Need to update this, not sure if newest fine tune model is always last in the list
+        new_model = sorted_fine_tunes[0]["fine_tuned_model"]
+        print(new_model)
+
+        return new_model
