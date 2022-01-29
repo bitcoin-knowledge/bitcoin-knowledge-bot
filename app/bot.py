bbbaa7c14fde25b842c9ed75108206415fa84755
austinkelsay <austinkelsay@yahoo.com>
2022-01-29 14:01:48 -0700

    Dropping temp and max tokens

--- app/bot.py
+++ app/bot.py
@@ -32,8 +32,8 @@ Bot: You're welcome, come back anytime!'''
 def ask(chat_log: str):
     prompt = f'{start_chat_log}\n\n###\n\n{chat_log}Bot:'
     response = completion.create(
-        prompt=prompt, model=model, stop=['\n\n###\n\n'], temperature=0.9,
-        frequency_penalty=1, presence_penalty=1, max_tokens=350)
+        prompt=prompt, model=model, stop=['\n\n###\n\n'], temperature=0.3,
+        frequency_penalty=1, presence_penalty=1, max_tokens=250)
     answer = response.choices[0].text.strip()
     
     return answer
