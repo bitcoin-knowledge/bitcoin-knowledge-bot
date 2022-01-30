a56cf50f959d953022841039402e664b7ce54439
austinkelsay <austinkelsay@yahoo.com>
2022-01-30 14:13:25 -0700

    changing bot version back to v7

--- app/bot.py
+++ app/bot.py
@@ -8,7 +8,7 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 completion = openai.Completion()
 
-model = os.getenv("FINE_TUNE_MODEL_V8")
+model = os.getenv("FINE_TUNE_MODEL_V7")
 
 
 start_chat_log = '''
