581a31e48b901c8427bd31b76dcaa8e026a812e6
austinkelsay <austinkelsay@yahoo.com>
2022-02-03 18:16:02 -0700

    Updating stop sequence for new dataset

--- app/bot.py
+++ app/bot.py
@@ -18,21 +18,22 @@ The assistant is helpful, creative, clever, friendly and it tries it's best to s
 
 
 User: How does proof-of-work work in Bitcoin?\n\n###\n\n
-Bot: A proof of work is a piece of data which is difficult (costly, time-consuming) to produce but easy for others to verify and which satisfies certain requirements. Producing a proof of work can be a random process with low probability so that a lot of trial and error is required on average before a valid proof of work is generated. Bitcoin uses the Hashcash proof of work system.\n\n###\n\n
-User: What is a hash?\n\n###\n\nBot: A hash is a function which takes a piece of data and converts it into a number.\n\n###\n\n
+Bot: A proof of work is a piece of data which is difficult (costly, time-consuming) to produce but easy for others to verify and which satisfies certain requirements. Producing a proof of work can be a random process with low probability so that a lot of trial and error is required on average before a valid proof of work is generated. Bitcoin uses the Hashcash proof of work system.\n
+User: What is a hash?\n\n###\n\n
+Bot: A hash is a function which takes a piece of data and converts it into a number.\n
 User: What is SHA-256?\n\n###\n\n
-Bot: SHA-256 is a specific hash function which takes a piece of data and converts it into a number.\n\n###\n\n
+Bot: SHA-256 is a specific hash function which takes a piece of data and converts it into a number.\n
 User: What is a block?\n\n###\n\n
-Bot: A block is a piece of data which contains a hash of the previous block and a set of transactions. These blocks make up the blockchain which is Bitcoin's distributed ledger with the full history and cryptographic proof of all of the previous transactions.\n\n###\n\n
+Bot: A block is a piece of data which contains a hash of the previous block and a set of transactions. These blocks make up the blockchain which is Bitcoin's distributed ledger with the full history and cryptographic proof of all of the previous transactions.\n
 User: do you have any trading advice for me?\n\n###\n\n
-Bot: Trading is risky and I dont give financial advice, I only suggest stacking Bitcoin and holding your own keys (Not your keys not your Bitcoin)\n\n###\n\n
+Bot: Trading is risky and I dont give financial advice, I only suggest stacking Bitcoin and holding your own keys (Not your keys not your Bitcoin)\n
 User: Thank you for the help!\n\n###\n\n
-Bot: You're welcome, come back anytime!'''
+Bot: You're welcome, come back anytime!\n'''
 
 def ask(chat_log: str):
     prompt = f'{start_chat_log}\n\n###\n\n{chat_log}Bot:'
     response = completion.create(
-        prompt=prompt, model=model, stop=['\n\n###\n\n'], temperature=0.4,
+        prompt=prompt, model=model, stop=['\n\n###\n\n', '\n'], temperature=0.4,
         frequency_penalty=1, presence_penalty=1, max_tokens=250)
     answer = response.choices[0].text.strip()
     
