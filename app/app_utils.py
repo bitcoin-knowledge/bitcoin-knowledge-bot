3d2272696e73e3b06c5e4a02703f21bac8c26330
austinkelsay <austinkelsay@yahoo.com>
2022-05-08 17:00:24 -0600

    Changing ping method to post a question that will hopefully require no tokens

--- app/app_utils.py
+++ app/app_utils.py
@@ -1,14 +1,10 @@
 import requests
 from pathlib import Path
+from app.bot import ask
 
 def get_project_root() -> Path:
     return Path(__file__).parent.parent
 
 
 def ping_fine_tune_model():
-    url = "https://api.openai.com/v1/fine-tunes/ft-u0IJDXpZGLdZyMGcFRH8pOKO"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return True
-    else:
-        return False
+    ask("ping")
